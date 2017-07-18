2a329b96faf4bcd61fc4bd940e4ea36f85683809
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 13:51:40 -0600

    relative link

--- index.php
+++ index.php
@@ -7,8 +7,12 @@
 		<?php include("header.html");?>
 			<main class="mdl-layout__content">
 				<div class="page-content">
-					<h1>Welcome!</h1>
-					<p>Hi, I'm currently a 5th year student at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
+					<h1>
+						Welcome!
+					</h1>
+					<p>
+						Hi, I'm currently a 5th year student at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.
+					</p>
 				</div>
 			</main>
 		</div>
