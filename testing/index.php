131b0fd8a3710efba86549f9928c399210c2eb69
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 12:30:43 -0600

    Fixed justification

--- testing/index.php
+++ testing/index.php
@@ -1,6 +1,19 @@
 <!DOCTYPE HTML>
 <html>
-		<?php include("header.html");?>
+	<head>
+	    <title>
+			Homepage
+		</title>
+        <style>
+			body {
+			width: 35em;
+			margin: 0 auto;
+			font-family: Tahoma, Verdana, Arial, sans-serif;
+			}
+        </style>
+		
+		
+	<?php include("header.html");?>
 		<h1>Welcome to my Homepage yay!</h1>
 		<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
 		<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
