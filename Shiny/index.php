7b7a55305cd36b529c93c6565777d21c5dbf3c30
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 13:30:26 -0600

    Added shiny

---
+++ Shiny/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Shiny Apps
+		</title>
+		<?php include("../header.html");?>
+			<main class="mdl-layout__content">
+				<div class="page-content">
+					<h1>
+						Shiny Apps
+					</h1>
+					<a href="http://grahamst.at">
+						Home
+					</a><br/>
+					<a href="http://grahamst.at/shiny/Simulation/">
+						Simulation
+					</a><br/>
+				</div>
+			</main>
+		</div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -7,24 +7,7 @@
 		<?php include("header.html");?>
 			<main class="mdl-layout__content">
 				<div class="page-content">
-					<h1>Welcome to my Homepage yay!</h1>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
-					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
+					<h1>Welcome!</h1>
 					<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
 				</div>
 			</main>
