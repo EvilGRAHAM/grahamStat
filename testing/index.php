986ef80dbc619fb9bb06d10edfadbca01d77f7d4
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 11:30:10 -0600

    Added test php

--- testing/index.php
+++ testing/index.php
@@ -22,6 +22,7 @@
         </style>
     </head>
     <body>
+		<?php include("includes/header.html");?>
 		<h1>Welcome to my Homepage</h1>
 		<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
 		<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
