0d9788b73cfe8fff084ec0025f7b7a8aa7806d26
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 14:30:51 -0600

    added cards to index

--- shiny/index.php
+++ shiny/index.php
@@ -5,16 +5,86 @@
 			Shiny Apps
 		</title>
 		<?php include("../header.html");?>
-			<main class="mdl-layout__content">
-				<div class="page-content">
-					<h1>
-						Shiny Apps
-					</h1>
-					<p>
-						testing
-					</p>
+			<div class="mdl-grid">
+			
+				<!-- Wide card with share menu button -->
+				<style>
+					.img-header-card.mdl-card {}
+					.img-header-card > .mdl-card__title {
+					  color: #000000;
+					  height: 176px;
+					  background: url('previews/vp_sim.PNG') center / cover;
+					}
+					.img-header-card > .mdl-card__menu {
+					  color: #000000;
+					}
+				</style>
+				<div class="mdl-cell mdl-cell--8-col mdl-cell--8-col-tablet img-header-card mdl-card mdl-shadow--2dp">
+					<div class="mdl-card__title">
+						<h2 class="mdl-card__title-text">
+							VP Simulation
+						</h2>
+					</div>
+					<div class="mdl-card__supporting-text">
+						Simulate and Predict Vapour Pressure based on Density, Sulfur and Temperature.
+					</div>
+					<div class="mdl-card__actions mdl-card--border">
+						<a 
+							class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" 
+							href = "http://grahamst.at/shiny-server/Simulation/"
+							target = "_blank"
+						>
+							Get Started
+						</a>
+					</div>
 				</div>
-			</main>
+				
+				<div class="mdl-card mdl-shadow--2dp">
+					<div class="mdl-card__title">
+						<h2 class="mdl-card__title-text">
+							VP Simulation
+						</h2>
+					</div>
+					<div class="mdl-card__media">
+						<img src="previews/vp_sim.PNG" width="220" height="140" border="0" alt="" style="padding:20px;">
+					</div>
+					<div class="mdl-card__supporting-text">
+						Simulate and Predict Vapour Pressure based on Density, Sulfur and Temperature.
+					</div>
+					<div class="mdl-card__actions">
+						<a 
+							class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" 
+							href = "http://grahamst.at/shiny-server/Simulation/"
+							target = "_blank"
+						>
+							Get Started
+						</a>
+					</div>
+				</div>
+				
+				<div class="mdl-card mdl-shadow--2dp">
+					<div class="mdl-card__title">
+						<h2 class="mdl-card__title-text">
+							VP Simulation
+						</h2>
+					</div>
+					<div class="mdl-card__supporting-text">
+						Simulate and Predict Vapour Pressure based on Density, Sulfur and Temperature.
+					</div>
+					<div class="mdl-card__actions">
+						<a 
+							class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" 
+							href = "http://grahamst.at/shiny-server/Simulation/"
+							target = "_blank"
+						>
+							Get Started
+						</a>
+					</div>
+				</div>
+				
+			</div>
+		
+		
 		</div>
     </body>
 </html>
