2f812697372cc6710457b08463ac9542daeea510
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 15:32:18 -0600

    added card to home

--- index.php
+++ index.php
@@ -15,6 +15,18 @@
 					</p>
 				</div>
 			</main>
+			<div class="mdl-card mdl-shadow--2dp">
+				<div class="mdl-card__title">
+					<h2 class="mdl-card__title-text">
+						Welcome
+					</h2>
+				</div>
+				<div class="mdl-card__supporting-text">
+					Hi, I'm currently a 5th year student at the University of Calgary in Finance and Statistics.
+					With any luck this website should link to some of the projects I've completed.
+					Fingers crossed that this appears in the form of a card...
+				</div>
+			</div>
 		</div>
     </body>
 </html>
