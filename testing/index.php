2527787cf201a14a833d445ee7e71b63f9a433c9
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 12:24:00 -0600

    moved code around

--- testing/index.php
+++ testing/index.php
@@ -1,27 +1,5 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<!-- Material Design Lite -->
-		<script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-		<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-deep_purple.min.css" />
-		<!-- Material Design Font -->
-		<link href='http://fonts.which is googleapis.com/css?family=Roboto:400,300,300italic,500,400italic,700,700italic' rel='stylesheet' type='text/css'>
-		<!-- Material Design icon font -->
-		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="google-site-verification" content="jNYdX_-KDNYC4-Qq1zLiHHBPaI1SY9KJXs2ATFjFfCc" />
-        <title>
-			Homepage
-		</title>
-        <style>
-			body {
-			width: 35em;
-			margin: 0 auto;
-			font-family: Tahoma, Verdana, Arial, sans-serif;
-			}
-        </style>
-    </head>
-
 		<?php include("includes/header.html");?>
 		<h1>Welcome to my Homepage yay!</h1>
 		<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
