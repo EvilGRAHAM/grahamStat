548e03726847540f72c2d6f7add52a15141434be
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 12:32:00 -0600

    moved lower hopefully

--- testing/index.php
+++ testing/index.php
@@ -11,9 +11,8 @@
 			font-family: Tahoma, Verdana, Arial, sans-serif;
 			}
         </style>
-		
-		
-	<?php include("header.html");?>
+		<?php include("header.html");?>
+	
 		<h1>Welcome to my Homepage yay!</h1>
 		<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
 		<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
