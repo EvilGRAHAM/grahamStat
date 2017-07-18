33f52850f5da65ef846b726a75d688587d8a98f1
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 12:19:37 -0600

    removed </body>

--- testing/index.php
+++ testing/index.php
@@ -21,7 +21,7 @@
 			}
         </style>
     </head>
-    <body>
+
 		<?php include("includes/header.html");?>
 		<h1>Welcome to my Homepage yay!</h1>
 		<p>Hi, I'm currently a 5th year studen at the University of Calgary in Finance and Statistics. With any luck this website should link to some of the projects I've completed.</p>
