78c7d9557623336fc95b760c560eb88f70683f83
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-19 12:13:12 -0600

    ugh

--- index.php
+++ index.php
@@ -8,57 +8,57 @@
 		
 			<div class="mdl-grid">
 
-				<div class="mdl-card mdl-shadow--2dp mdl-cell">
+				<div class="mdl-card mdl-card--border mdl-shadow--2dp mdl-cell">
 					<div class="mdl-card__title">
 						<h2 class="mdl-card__title-text">
 							Welcome!
 						</h2>
 					</div>
-					<div class="mdl-card__supporting-text mdl-card--border">
+					<div class="mdl-card__supporting-text">
 						Hi, welcome to my site! With any luck this website should link to some of the projects I've completed. Fingers crossed that this appears in the form of a card...
 					</div>
 				</div>
 				
-				<div class="mdl-card mdl-shadow--2dp mdl-cell">
+					<div class="mdl-card mdl-card--border mdl-shadow--2dp mdl-cell">
 					<div class="mdl-card__title">
 						<h2 class="mdl-card__title-text">
 							Content:
 						</h2>
 					</div>
-					<div class="mdl-card__supporting-text mdl-card--border">
+					<div class="mdl-card__supporting-text">
 						This site currently is host to a R Studio Server, as well as a Shiny Server. The goal is to be able to host shiny applications and R Markdown docs for the different projects I'm working on or have completed. As well this is my first time at doing web development, so we'll see how well that goes...
 					</div>
 				</div>
 				
-				<div class="mdl-card mdl-shadow--2dp mdl-cell">
+				<div class="mdl-card mdl-card--border mdl-shadow--2dp mdl-cell">
 					<div class="mdl-card__title">
 						<h2 class="mdl-card__title-text">
 							Education:
 						</h2>
 					</div>
-					<div class="mdl-card__supporting-text mdl-card--border">
+					<div class="mdl-card__supporting-text">
 						I'm currently a 5th year undergraduate at the University of Calgary in Finance and Statistics. I'm on track to graduate in the Winter 2018 semester with a Bachelor of Commerce and a Bachelor of Science.
 					</div>
 				</div>
 				
-				<div class="mdl-card mdl-shadow--2dp mdl-cell">
+				<div class="mdl-card mdl-card--border mdl-shadow--2dp mdl-cell">
 					<div class="mdl-card__title">
 						<h2 class="mdl-card__title-text">
 							Programming:
 						</h2>
 					</div>
-					<div class="mdl-card__supporting-text mdl-card--border">
+					<div class="mdl-card__supporting-text">
 						I do most of my work in R, with an emphasis on the <a href="http://tidyverse.org/">tidyverse</a> packages. As well I've played around with <a href="https://plot.ly/">Plotly</a> and <a href="https://shiny.rstudio.com/">Shiny</a>. Additionally I'm familiar with T-SQL, having built a data warehouse of sorts with it from scratch in Microsoft Azure, Python 3, some VBA, and I'm learning HTML as I build this site. I don't know if you can really call Latex a language, but I've used that as well for school.
 					</div>
 				</div>
 				
-				<div class="mdl-card mdl-shadow--2dp mdl-cell">
+				<div class="mdl-card mdl-card--border mdl-shadow--2dp mdl-cell">
 					<div class="mdl-card__title">
 						<h2 class="mdl-card__title-text">
 							Backend:
 						</h2>
 					</div>
-					<div class="mdl-card__supporting-text mdl-card--border">
+					<div class="mdl-card__supporting-text">
 						This site is currently supported through the use of a "droplet" from <a href="https://www.digitalocean.com/">Digital Ocean</a>. They're super cheap and easy to run, so I'd highly recommend them if you need a low cost backend.
 					</div>
 				</div>
