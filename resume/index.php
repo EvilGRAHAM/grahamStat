73ebee688bfe3b0d13f5c5042da1a90d6afdb7cf
EvilGRAHAM <scott.grah95@gmail.com>
2017-08-30 14:32:06 -0600

    reformatted

--- resume/index.php
+++ resume/index.php
@@ -16,34 +16,17 @@
 					</div>
 					<div class="mdl-card__supporting-text">
 					
-						<p>
-							24 Mt Kidd Pt. SE
-							<br>
-							Calgary, Alberta T2Z 3C5
-							<br>
-							(403) 481-5609
-							<br>
-							scott.grah95@gmail.com
-						</p>
-
-
-
-						<h1>
-							Scott Graham
-						</h1>
-
-
-						<h2>
+						<h3>
 							Objective:
-						</h2>
+						</h3>
 						<p>
 							To gain employment with Tundra Energy Marketing as a Marketing and Trading Summer Student.
 						</p>
 
 
-						<h2>
+						<h3>
 							Education:
-						</h2> 
+						</h3> 
 						<p>
 							Sep. 2013 - Present
 							University of Calgary 
