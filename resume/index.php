2dfd96ca5d06c676b52090b6d552c1e75adf2421
EvilGRAHAM <scott.grah95@gmail.com>
2017-08-30 14:36:17 -0600

    different way of formatting

--- resume/index.php
+++ resume/index.php
@@ -11,33 +11,41 @@
 				<div class="mdl-card mdl-shadow--2dp mdl-cell">
 					<div class="mdl-card__title">
 						<h2 class="mdl-card__title-text">
-							Résumé
+							Education:
 						</h2>
 					</div>
 					<div class="mdl-card__supporting-text">
-					
-						<h3>
-							Objective:
-						</h3>
-						<p>
-							To gain employment with Tundra Energy Marketing as a Marketing and Trading Summer Student.
-						</p>
-
-
-						<h3>
-							Education:
-						</h3> 
 						<p>
 							Sep. 2013 - Present
 							University of Calgary 
 							Finance/Statistics Programs
 							3.629 GPA
 							Calgary, Alberta 
+							
+							Sep. 2010 - Jun. 2013
+							Bishop O’Byrne High School
+							Grade 12
+							International Baccalaureate Programme
+							Calgary, Alberta
 						</p>
-
 					</div>
 				</div>
 				
+				<div class="mdl-card mdl-shadow--2dp mdl-cell">
+					<div class="mdl-card__title">
+						<h2 class="mdl-card__title-text">
+							Work Experience:
+						</h2>
+					</div>
+					<div class="mdl-card__supporting-text">
+						<p>
+							May. 2016 – Sep. 2017
+							Marketing & Trading Summer Student
+							Tundra Energy Marketing Limited
+							Calgary, Alberta
+						</p>
+					</div>
+				</div>
 				
 			</div>
 		</div>
