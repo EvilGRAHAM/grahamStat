53396badd40f89aa30330a6db41fe52ec1e68a7b
Scott Graham <scott.grah95@gmail.com>
2017-12-16 10:24:54 -0700

    fixed link

--- blog/index.php
+++ blog/index.php
@@ -13,12 +13,12 @@
 					</h2>
 				</div>
 				<div class="mdl-card__supporting-text">
-					This is written in response to this reddit post, talking about how Chris Sale tends to fall off in later months. When looking at the data, the relationship looked tenuous at best, so I wanted to apply some actual statistical tests to see if such a relationship exists.
+					This is written in response to <a href = "https://www.reddit.com/r/baseball/comments/7k12ua/why_is_it_controversial_to_point_out_that_chris/" target = "_blank">this reddit post</a>, talking about how Chris Sale tends to fall off in later months. When looking at the data, the relationship looked tenuous at best, so I wanted to apply some actual statistical tests to see if such a relationship exists.
 				</div>
 				<div class="mdl-card__actions mdl-card--border">
 					<a 
 						class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect" 
-						href = "https://m.do.co/c/71ac9cc4daaa"
+						href = "2017_12_15_Chris_Sale"
 						target = "_blank"
 					>
 						Go
