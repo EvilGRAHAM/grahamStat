c9fe82538fd89865822188df54458891270cbc5e
Scott Graham <scott.grah95@gmail.com>
2017-12-16 10:21:52 -0700

    added the Sale Splits blog post

--- blog/index.php
+++ blog/index.php
@@ -6,6 +6,26 @@
 		</title>
 		<?php include("../header.html");?>
 		
+			<div class="mdl-card mdl-shadow--2dp mdl-cell">
+				<div class="mdl-card__title">
+					<h2 class="mdl-card__title-text">
+						Chris Sale Splits
+					</h2>
+				</div>
+				<div class="mdl-card__supporting-text">
+					This is written in response to this reddit post, talking about how Chris Sale tends to fall off in later months. When looking at the data, the relationship looked tenuous at best, so I wanted to apply some actual statistical tests to see if such a relationship exists.
+				</div>
+				<div class="mdl-card__actions mdl-card--border">
+					<a 
+						class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect" 
+						href = "https://m.do.co/c/71ac9cc4daaa"
+						target = "_blank"
+					>
+						Go
+					</a>
+				</div>
+			</div>
+		
 		</div>
     </body>
 </html>
