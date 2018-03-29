49fb619a8eab8dfaa066afc5a6f165ead4d35c9e
Scott Graham <scott.grah95@gmail.com>
2018-03-29 14:05:48 -0600

    added monster builder

--- projects/index.php
+++ projects/index.php
@@ -62,6 +62,40 @@
 				<div class="mdl-card mdl-shadow--2dp mdl-cell">
 					<div class="mdl-card__title">
 						<h2 class="mdl-card__title-text">
+							D&D Monster Builder
+						</h2>
+					</div>
+					<div class="mdl-card__supporting-text">
+						A small shiny app that builds monsters, and calculates CR according to the DMG.
+					</div>
+					<div class="mdl-card__actions mdl-card--border">
+						<a 
+							class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect" 
+							href = "http://grahamst.at/shiny-server/DnD_Monster_Builder/"
+							target = "_blank"
+						>
+							Get Started
+						</a>
+						<a 
+							class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect" 
+							href = "http://grahamst.at/shiny-server/DnD_Monster_Builder/?_inputs_&armor_type="Natural%20Armor"&blindsight=0&burrow=0&cha=6&climb=0&con=13&cond_immune=null&darkvision=0&dex=14&expected_cr="0.25"&fly=0&good_evil=""&hit_dice=3&immune=null&int=4&lang=null&law_chaos=""&multiattack=1&name="Velociraptor"&nat_armor=1&resist=null&saves=null&shield=false&size="t"&skills="Perception"&speed=30&str=6&swim=0&tremorsense=0&truesight=0&type="Beast"&vuln=null&weapon_type=%5B"Shortsword"%2C"Dagger"%5D&wis=12"
+							target = "_blank"
+						>
+							Velociraptor
+						</a>
+						<a 
+							class="mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect" 
+							href = "http://grahamst.at/shiny-server/DnD_Monster_Builder/?_inputs_&armor_type=%22Natural%20Armor%22&blindsight=0&burrow=0&cha=5&climb=0&con=18&cond_immune=%5B%22Charmed%22%2C%22Exhaustion%22%2C%22Frightened%22%2C%22Paralyzed%22%2C%22Petrified%22%2C%22Poisoned%22%5D&darkvision=60&dex=9&expected_cr=5&fly=0&good_evil=%22Neutral%22&hit_dice=11&immune=%5B%22Lightning%22%2C%22Poison%22%2C%22Bludgeoning%22%2C%22Piercing%22%2C%22Slashing%22%5D&int=6&lang=null&law_chaos=%22Neutral%22&multiattack=2&name=%22Flesh%20Golem%22&nat_armor=0&resist=null&saves=null&shield=false&size=%22m%22&skills=null&speed=30&str=19&swim=0&tremorsense=0&truesight=0&type=%22Undead%22&vuln=null&weapon_type=%22Greatsword%22&wis=10"
+							target = "_blank"
+						>
+							Flesh Golem
+						</a>
+					</div>
+				</div>
+				
+				<div class="mdl-card mdl-shadow--2dp mdl-cell">
+					<div class="mdl-card__title">
+						<h2 class="mdl-card__title-text">
 							D&D Insult Generator
 						</h2>
 					</div>
